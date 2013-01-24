e233cc0ba9989d8ea856725b83bdb57646eba4da
jfjcn <jfjcn@hotmail.com>
2013-01-24 03:43:49 -0700

    Added in a new file for saving "Albums" to test my GIT skills

---
+++ src/WebApiExamples.Services/CDCollection/AlbumDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebApiExamples.Services.CDCollection
+{
+    public class AlbumDataService
+    {
+    }
+}
